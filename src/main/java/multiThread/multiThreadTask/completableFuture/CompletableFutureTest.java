cbe0bdc9d4941cf33fa63ff229543380e1d7d784
zoujialei <570035853@qq.com>
2023-01-31 17:41:56 +0800

    CompletableFuture总结-补充

--- src/main/java/multiThread/multiThreadTask/completableFuture/CompletableFutureTest.java
+++ src/main/java/multiThread/multiThreadTask/completableFuture/CompletableFutureTest.java
@@ -10,7 +10,14 @@ import java.util.concurrent.*;
  * 可见 (最终依赖系统提供的方法，阻塞相应的时间，看到系统层就先不用看了)
  * {@link multiThread.LockSupport.LockSupportDemo}
  *
- * 1.相对于ListenableFuture来说，优势就是一点：可组合，可编排，这一点的美团技术文章见https://tech.meituan.com/2022/05/12/principles-and-practices-of-completablefuture.html
+ * 1.相对于ListenableFuture来说，优势就是一点：可组合，可编排，这一点的美团技术文章有说明：
+ * https://tech.meituan.com/2022/05/12/principles-and-practices-of-completablefuture.html
+ * 简单总结
+ * 1）数据结构：每个CF都有一个result，用户保存执行结果，有一个stack，用户保存依赖该CF的其他0个或多个CF。
+ * 2）观察者模式通知：对于异步任务，当前CF执行完之后，如何通知后续需要执行的CF：每个和当前线程相关的CF，都在关联的时候写入到了当前线程的栈中，当前线程执行完后，就通知所有栈中CF；
+ * 3）对于文章提及的多元依赖，为啥多个被依赖是AndTree，而不是链表或数据：主要用下二元依赖的数据结构，就不用在单独引入其他数据结构了，例如链表、数组等还要在写一套时间。
+ * 二元依赖的结构，如果是两个依赖都ready才执行Action，两个左右节点的根节点才会ready，这样一层一层向上运算，遍完成了整个树中所有节点的遍历。
+ *
  * 2.本文章基本把如何使用将清楚了：https://juejin.cn/post/7100402930083168292
  * 简单总结下：
  * 1）runAsync和supplyAsync分别对应Runnable和Supplier接口，Run没有返回值，supply有返回值
